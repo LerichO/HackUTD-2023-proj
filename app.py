42991cb2f666b67343a10aac80ac87ddb8299b07
LerichO <lerichyaso@gmail.com>
2023-11-04 22:00:53 -0500

    started app.py

--- app.py
+++ app.py
@@ -1,9 +1,42 @@
-from flask import Flask, render_template
+
+# import proper libraries
+from flask import Flask, render_template, request
+from flask_pymongo import PyMongo # for later
+import model
+import math
+import os
+
+# initialization
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
+monthlyCarPayment = 0
+studentLoanPayment = 0
+estMonthlyMortgagePayment = 0
+creditScore = 0
+grossMonthlyIncome = 0
+monthlyCreditCardPayment = 0
+homeAppraisedValue = 0
+downPaymentAmount = 0
+
+# -- Routing --
+
+# -- main page --
+# do we need the get request?
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method() == "POST":
+        # take in data from questionnare thing
+        # print message placeholder
+        print("Nice")
+        return render_templet("index.html")
+    else:
+        return render_template("index.html")
+
+# # eventually to rename this page or specialize it
+# @app.route("/stats", methods=["GET", "POST"])
+# def stats():
+
+
 
 if __name__ == "__main__":
     app.run()
